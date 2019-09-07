47a747d5f844202863609ae78e0ed4dbabf5d4e1
Franco <fnastasi@fi.uba.ar>
2019-09-07 19:09:07 -0300

    Se escribió la función Eul2RMat

--- tp1.py
+++ tp1.py
@@ -20,8 +20,34 @@ Ejemplo de como utilizar la función para phi = 45°, tita = 45° y psi =0° :
     (R,sg_tita) = Eul2RMat(phi =45,tita=45,psi=0 )
 """
 
-def Eul2RMat(phi=0, tita=0,phi=0):
-    return R,sg_tita
+
+def Eul2RMat(phi=0, tita=0,psi=0):
+    # los ángulos se pasan a radianes
+    phi = phi*pi/180
+    tita = tita*pi/180
+    psi = psi*pi/180
+    
+    # Se escriben las matrices de rotación y con la función array se convierten en elementos
+    # de numpy
+    R_z_phi = [[cos(phi), -sin(phi), 0],[sin(phi), cos(phi), 0],[0,0,1]]
+    R_z_phi = array(R_z_phi)
+    #print(R_z_phi)
+    
+    R_y_tita = [[cos(tita), 0, sin(tita)], [0,1,0] , [-sin(tita), 0, cos(tita)]]
+    R_y_tita = array(R_y_tita)
+    #print(R_y_tita)
+    
+    R_z_psi = [[cos(psi), -sin(psi), 0],[sin(psi), cos(psi), 0],[0,0,1]]
+    R_z_psi = array(R_z_psi)
+    #print(R_z_psi)
+    
+    # Matriz de rotación calculada como el producto de las 3 matrices
+    R = linalg.multi_dot([R_z_phi,R_y_tita,R_z_psi])
+    # El índice de configuración
+    sg_tita = sign(tita)
+    return around(R,decimals=5),sg_tita
+
+
 
 
 # In[]
